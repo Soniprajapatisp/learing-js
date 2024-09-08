5327f8b036f5f835714b211a9e36fa4ef796bad6
Soniprajapatisp <sonisp@proton.me>
2024-09-08 09:18:20 +0000

    final

---
+++ script.js
@@ -0,0 +1,70 @@
+alert("hey");
+// console.log("Hello I am soni here");
+
+// let testString = "Hello Soni";
+// let x;
+// x = testString.toUpperCase();
+// x = testString.startsWith("Re");
+// x = testString.repeat(3);
+// x = testString.indexOf(2);
+// x=  testString.charAt(2);
+// x = testString.replaceAll("o", "e");
+// x = testString.slice(6,10);
+
+
+// console.log(x);
+
+// function repeatString("String" , Number){
+//     return  String.repeat(Number)   
+// }
+// const y= repeatString("hey", 2)
+// y = String.repeat(2);
+// console.log(y);
+
+// function capitize(word){
+// const firstchar =word.at(0).toUpperCase;
+// const test = word.slice(1).tolowerCase;
+// return firstchar + test;
+// }
+
+// function capitizeName(name){
+//     const spaceInx = name.indexOf(' ');
+//     const firstName = name.slice(0,spaceInx );
+//     const lastName = name.slice(spaceInx +1);
+//     return capitize(firstName) + capitize(lastName);
+// }
+// console.log(capitizeName("john WORLD"))
+
+
+// // const age =15
+// function greet("bob", 15){
+//     if age>=15{
+//     console.log("Hey Bob, you're a minor");
+//     }
+// }
+
+
+// for (let i = 1; i<=100; i++);
+// console.log(i);
+
+function printTable(number){
+    for (let i = 1, i<=10, i++){
+        const str = Number + "*" +1+"=" +1*number
+    }
+        console.log(i);
+
+}
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+
